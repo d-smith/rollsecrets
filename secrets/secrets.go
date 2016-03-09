6ff7ee134a7414e5c80395be207de1dda85ba176
d-smith <doug.smith@fmr.com>
2016-03-09 09:59:03 -0800

    Added secrets repo and implementation
    Signed-off-by: d-smith <doug.smith@fmr.com>

---
+++ secrets/secrets.go
@@ -0,0 +1,9 @@
+package secrets
+
+//SecretsRepo defines a repository abstraction for reading and writing secrets from a secure
+//secret store
+type SecretsRepo interface {
+	StoreKeysForApp(appkey string, privateKey string, publicKey string) error
+	RetrievePrivateKeyForApp(appkey string) (string, error)
+	RetrievePublicKeyForApp(appkey string) (string, error)
+}
